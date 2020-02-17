845ce05010d82341c2e7bd1ae341bb311008b75f
Tim Wheeler <timothy.wheeler@virginpulse.com>
2020-02-16 23:10:23 -0500

    refactored counter app to use reducer for state management

--- rn-starter/src/screens/CounterScreen.js
+++ rn-starter/src/screens/CounterScreen.js
@@ -1,37 +1,52 @@
-import React, { useState } from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import React, { useReducer } from 'react';
+import {View, Text, Button, StyleSheet} from 'react-native';
+
+const isPositiveNumber = (counter, payload) => {
+    return (counter + payload) >= 0;
+};
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'INCREMENT_COUNTER':
+            return isPositiveNumber(state.counter, action.payload) ? { ...state, counter: state.counter + action.payload } : state;
+        case 'DECREMENT_COUNTER':
+            return isPositiveNumber(state.counter, action.payload) ? { ...state, counter: state.counter + action.payload } : state;
+        case 'INCREMENT_OTHER_COUNTER':
+            return isPositiveNumber(state.otherCounter, action.payload) ? { ...state, otherCounter: state.otherCounter + action.payload } : state;
+        case 'DECREMENT_OTHER_COUNTER':
+            return isPositiveNumber(state.otherCounter, action.payload) ? { ...state, otherCounter: state.otherCounter + action.payload } : state;
+        default:
+            return state;
+    }
+};
+
+const incrementCounter = () => ({type: 'INCREMENT_COUNTER', payload: 1});
+const decrementCounter = () => ({type: 'DECREMENT_COUNTER', payload: -1});
+const incrementOtherCounter = () => ({type: 'INCREMENT_OTHER_COUNTER', payload: 1});
+const decrementOtherCounter = () => ({type: 'DECREMENT_OTHER_COUNTER', payload: -1});
 
 const CounterScreen = () => {
-
-  const [counter, setCounter] = useState(0);
-  const [otherCounter, setOtherCounter] = useState(100);
-
-  return (
-      <View>
-        <Button title={ 'Increase' } onPress={ () => {
-          setCounter(counter + 1);
-        } }/>
-        <Button title={ 'Decrease' } onPress={ () => {
-          setCounter(counter - 1);
-        } }/>
-        <Text style={ styles.textStyle }>Count: { counter }</Text>
-        <Button title={ 'Increase' } onPress={ () => {
-          setOtherCounter(otherCounter + 1);
-        } }/>
-        <Button title={ 'Decrease' } onPress={ () => {
-          setOtherCounter(otherCounter - 1);
-        } }/>
-        <Text style={ styles.textStyle }>Other Count: { otherCounter }</Text>
-      </View>
-  )
+    const initialState = {counter: 0, otherCounter: 0};
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const {counter, otherCounter} = state;
+    return (
+        <View>
+            <Button title={'Increase'} onPress={() => dispatch(incrementCounter())}/>
+            <Button title={'Decrease'} onPress={() => dispatch(decrementCounter())}/>
+            <Text style={styles.textStyle}>Count: {counter}</Text>
+            <Button title={'Increase'} onPress={() => dispatch(incrementOtherCounter())}/>
+            <Button title={'Decrease'} onPress={() => dispatch(decrementOtherCounter())}/>
+            <Text style={styles.textStyle}>Other Count: {otherCounter}</Text>
+        </View>
+    )
 };
 
 const styles = StyleSheet.create({
-  textStyle: {
-    fontSize: 30,
-    textAlign: 'center',
-    marginVertical: 20
-  }
+    textStyle: {
+        fontSize: 30,
+        textAlign: 'center',
+        marginVertical: 20
+    }
 });
 
 export default CounterScreen;
